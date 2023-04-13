0106e8b423d5f75d0477b9e497bf3734c655943c
Andrei <andrei_king1@yahoo.com>
2023-04-13 15:29:47 +0300

    Added more fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Archivo&family=Cherry+Cream+Soda&family=Figtree:wght@700&family=Inter:wght@300;400;600;900&family=Poppins&family=Roboto:wght@100;400;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Archivo:wght@400;900&family=Cherry+Cream+Soda&family=Figtree:wght@700&family=Inter:wght@300;400;600;900&family=Poppins&family=Roboto:wght@100;400;700&display=swap"
           rel="stylesheet"
         />
       </Head>
