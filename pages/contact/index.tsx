ec7fed2330c65669eea7c114d906479357ba9c8c
Andrei <andrei_king1@yahoo.com>
2023-05-06 15:37:32 +0300

    Contact page design

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -5,18 +5,8 @@ export default function Contact() {
   return (
     <div className={styles.container}>
       <div className={styles.overlay}></div>
-      <h2>Reach out to us!</h2>
-      <p>
-        <BsTelephone />
-        Phone: 0924-232-000
-      </p>
-      <p>
-        <MdOutlineMail /> Email: test2217@yahoo.com
-      </p>
-      <p>
-        {" "}
-        <MdOutlineLocationCity /> Address: New Avenue Street nr. 2, N.Y.
-      </p>
+      <h2>Reach out to us</h2>
+
       <div className={styles.containerInputs}>
         <div>
           <input placeholder="Your name" />
@@ -26,10 +16,27 @@ export default function Contact() {
         <div>
           <textarea placeholder="Your message" />
         </div>
+        <button>
+          <span>Send a message</span>
+        </button>
+        <div className={styles.bottomText}>
+          Company won&apos;t share, sell or trade customer information. Your privacy is important to
+          us.
+        </div>
+        <div className={styles.info}>
+          <p>
+            <BsTelephone />
+            Phone: 0924-232-000
+          </p>
+          <p>
+            <MdOutlineMail /> Email: test2217@yahoo.com
+          </p>
+          <p>
+            {" "}
+            <MdOutlineLocationCity /> Address: New Avenue Street nr. 2, N.Y.
+          </p>
+        </div>
       </div>
-      <button>
-        <span>Send a message</span>
-      </button>
     </div>
   );
 }
