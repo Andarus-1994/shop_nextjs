3f7d0b50308277bed8dd99cf288c21c21ac653e7
Andrei <andrei_king1@yahoo.com>
2023-05-10 18:20:18 +0300

    Products page changes filters

--- pages/items/index.tsx
+++ pages/items/index.tsx
@@ -1,13 +1,15 @@
 import styles from "../../styles/Items.module.scss";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { CSSProperties, useEffect, useState } from "react";
 import Image from "next/image";
 import JacketImage from "../../public/jacket.jpg";
 import Jeans from "../../public/jeans.jpg";
 import { AiOutlineSearch, AiOutlineLoading3Quarters } from "react-icons/ai";
 import { FaBoxes } from "react-icons/fa";
+import { MdOutlineFilterAlt } from "react-icons/md";
 import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai";
 import stylesUtils from "../../styles/utils/Loading.module.scss";
+import CreatableSelect from "react-select/creatable";
 
 interface Item {
   id: number;
@@ -36,6 +38,26 @@ export default function Items() {
       name: "Accessories",
     },
   ]);
+  const optionsBrand = [
+    { value: "Adidas", label: "Adidas" },
+    { value: "Puma", label: "Puma" },
+    { value: "Crocs", label: "Crocs" },
+  ];
+  const optionsColor = [
+    { value: "Red", label: "Red" },
+    { value: "Green", label: "Green" },
+    { value: "Black", label: "Black" },
+  ];
+  const optionsSize = [
+    { value: "S", label: "S" },
+    { value: "M", label: "M" },
+  ];
+  const optionsPrice = [
+    { value: "50", label: "Under 50 $" },
+    { value: "100", label: "Under 100 $" },
+    { value: "200", label: "Under 200 $" },
+  ];
+
   const fetchItems = async () => {
     const token = localStorage.getItem("token");
     const config = {
@@ -59,7 +81,7 @@ export default function Items() {
 
   return (
     <div className={styles.container}>
-      <div className={styles.filterSection}>
+      <div className={styles.categoriesSection}>
         {categoryList.length > 0 &&
           categoryList.map((category) => (
             <div key={category.id}>
@@ -78,17 +100,37 @@ export default function Items() {
           ))}
       </div>
       <div className={styles.containerItems}>
-        <div className={styles.filter}>
-          <div>
-            <label>Filters</label>
-            <button>Pants</button>
-            <button>Shirts</button>
-            <button className={styles.active}>Jackets</button>
-            <button>Blussons</button>
-            <button>Jackets</button>
-            <button>Jackets</button>
-            <AiOutlineSearch />
-            <input placeholder={" Search"} />
+        <div className={styles.filterSection}>
+          <div className={styles.itemsCountSort}>
+            <div className={styles.count}>{items.length} items found</div>
+            <div className={styles.sort}>
+              <label>Sort By:</label>{" "}
+              <select>
+                <option>Popular</option>
+                <option>Date Added</option>
+              </select>
+            </div>
+          </div>
+          <div className={styles.filters}>
+            <div>
+              <div className={styles.select}>
+                <CreatableSelect isClearable placeholder="Brand" options={optionsBrand} />
+              </div>
+              <div className={styles.select}>
+                <CreatableSelect placeholder="Color" isClearable options={optionsColor} />
+              </div>
+              <div className={styles.select}>
+                <CreatableSelect placeholder="Size" isClearable options={optionsSize} />
+              </div>
+              <div className={styles.select}>
+                <CreatableSelect placeholder="Price" isClearable options={optionsPrice} />
+              </div>
+            </div>
+            <div>
+              <button>
+                Reset Filters <MdOutlineFilterAlt />
+              </button>
+            </div>
           </div>
         </div>
         {isLoading ? (
