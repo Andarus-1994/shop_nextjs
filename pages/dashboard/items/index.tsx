7f923337a6e17968209b2492d821c8e61598861f
Andrei <andrei_king1@yahoo.com>
2023-05-21 15:27:31 +0300

    Dashboard items -> Get main categories methods

--- pages/dashboard/items/index.tsx
+++ pages/dashboard/items/index.tsx
@@ -1,18 +1,23 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { IndexLayout } from "../../../Components/LayoutDashboard";
 import styles from "../../../styles/Dashboard/Items.module.scss";
 import Item from "./item";
 import { itemsDashboard, itemsDashboard2 } from "../../../Components/Data/items";
 import NewCategory from "./newCategoryModal";
 import NewMainCategory from "./newMainCategoryModal";
+import Select from "react-select";
+import makeAnimated from "react-select/animated";
+import axios from "axios";
 
 export default function Items() {
   const [categories, setCategories] = useState([
     { id: "2", name: "Shirts" },
     { id: "3", name: "Backpacks" },
   ]);
+  const [mainCategories, setMainCategories] = useState([]);
   const [showMainCategoryModal, setShowMainCategoryModal] = useState(false);
   const [showCategoryModal, setShowCategoryModal] = useState(false);
+  const [loadingMainCategories, setLoadingMainCategories] = useState(false);
   const editItem = (i: number) => {
     console.log(i);
   };
@@ -22,6 +27,37 @@ export default function Items() {
   const [items, setItems] = useState(itemsDashboard);
   const [category, setCategory] = useState(categories[0].name);
   const [filterItem, setFilterItem] = useState("");
+  const animatedComponents = makeAnimated();
+
+  const getMainCategories = useCallback(async () => {
+    setLoadingMainCategories(true);
+    let errorMessage = "";
+    const token = localStorage.getItem("token");
+    const config = {
+      headers: { Authorization: `Bearer ${token}` },
+    };
+    try {
+      const items = await axios.get(
+        process.env.NEXT_PUBLIC_API_URL + "api/dashboard/getMainCategories",
+        config
+      );
+      const itemsData = items.data;
+      setMainCategories(itemsData);
+      console.log(itemsData);
+    } catch (e: unknown) {
+      if (e instanceof Error) {
+        errorMessage = e.message;
+        console.log(errorMessage);
+        setMainCategories([]);
+      }
+    } finally {
+      setLoadingMainCategories(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    getMainCategories();
+  }, [getMainCategories]);
 
   useEffect(() => {
     if (category === "Backpacks") {
@@ -57,9 +93,29 @@ export default function Items() {
         </div>
         <div>
           <label>Main Category</label>
-          <select>
-            <option>test</option>
-          </select>
+          <Select
+            backspaceRemovesValue={true}
+            captureMenuScroll={true}
+            isLoading={loadingMainCategories}
+            loadingMessage={() => "Loading"}
+            onChange={(e) => {
+              console.log(e);
+            }}
+            styles={{
+              control: (baseStyles, state) => ({
+                ...baseStyles,
+                width: "250px",
+                margin: "5px 0 0 0",
+                padding: "0px 10px",
+                border: "none",
+                borderRadius: "15px",
+                boxShadow: "0px 0px 3px rgba(0, 0, 0, 0.3)",
+              }),
+            }}
+            placeholder="Main Categories"
+            components={animatedComponents}
+            options={mainCategories}
+          />
         </div>
         <div>
           <button onClick={() => setShowMainCategoryModal(true)}>Create Main Category</button>
